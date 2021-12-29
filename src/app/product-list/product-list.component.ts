86dc085cf1487922c082d23712ff873085261c65
timothyAgevi <timothyagevi@gmail.com>
2021-12-29 14:44:46 +0300

    add productservice on productlist.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'app/product';
+import { ProductService } from '../../../product.service';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -8,7 +10,7 @@ import { Product } from 'app/product';
 export class ProductListComponent implements OnInit {
   products:Product[];
 
-  constructor() { }
+  constructor(private _productService:ProductService) { }
 
   ngOnInit() {
   }

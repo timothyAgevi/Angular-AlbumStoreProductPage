13bea4a9c499629344ba66774851de967ed8f933
timothyAgevi <timothyagevi@gmail.com>
2021-12-29 12:18:55 +0300

    productService request

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { ProductService } from '../product.service';
+import { ProductService } from '../product.service';
 
-import { ProductService } from '../../../product.service';
+
 
 import { Album } from '../album';
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 
-import {Http, Response} from "@angular/http";
+import { Http, Response } from "@angular/http";
 import { Album } from "./album";
 
 import'rxjs/add/operator/map'
@@ -19,7 +19,7 @@ export class ProductService{
     }
 
     getAlbum(id:number):Observable<Album> {
-        return this._http.get(this._albumUrl).map(response=> 
+        return this._http.get(this._albumUrl).map((response)=> 
             <Album>response.json());
     }
   

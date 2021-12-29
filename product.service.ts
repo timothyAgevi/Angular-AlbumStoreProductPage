f3d0fee140f83ffcdc1943ae2f3b3d9580e14b7d
timothyAgevi <timothyagevi@gmail.com>
2021-12-29 11:28:26 +0300

    generate service product

---
+++ product.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+
+import {Http, Response} from "@angular/http";
+import { Album } from "app/album";
+
+import'rxjs/add/operator/map'
+
+// import {Album } from "app/album";
+
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class ProductService{
+
+    private _albumUrl = '../assets/album.json';
+
+    constructor(private _http: Http ){
+
+    }
+
+    getAlbum(id:number):Observable<Album> {
+        return this._http.get(this._albumUrl).map(response=> 
+            <Album>response.json());
+    }
+  
+
+
+}
+
+
+

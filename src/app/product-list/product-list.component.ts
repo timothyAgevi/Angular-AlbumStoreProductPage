3644258bf137a13af277317376247ee09c6a0992
timothyAgevi <timothyagevi@gmail.com>
2022-01-03 23:45:12 +0300

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'app/product';
+import { Product } from '../product';
 import { ProductService } from '../product.service';
 @Component({
   selector: 'app-product-list',
